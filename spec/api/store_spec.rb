83877144a210baa6089c834b275f6aac1c12c5e5
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-09-15 20:33:32 +0200

    [rspec] adds red test for filtered get stores requests

--- spec/api/store_spec.rb
+++ spec/api/store_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe 'Store API' do
       expect(json['stores'].size).to eq(3)
     end
 
+    it 'returns stores matching filtered title' do
+      non_matching_stores = StoreFactory.create_stores(2)
+      matching_stores     = StoreFactory.create_stores(1, { title: "Yamaha" })
+
+      get '/stores', { title: "eq:Yamaha" }
+      stores_ids = JSON.parse(response.body)['stores'].map { |s| s['id'] }
+
+      expect(stores_ids.size).to eq(1)
+      expect(stores_ids).to include(matching_stores)
+      expect(stores_ids).to_not include(non_matching_stores)
+    end
+
   end
 end
