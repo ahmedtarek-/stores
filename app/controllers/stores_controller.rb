df1926da690e760264108b6f7d77317db80d2f53
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-09-15 20:04:59 +0200

    [controller] sets stores before index

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,12 @@
 class StoresController < ApplicationController
+  before_action :set_stores, only: [:index]
+
   def index
+  end
+
+  private
+
+  def set_stores
+    @stores = Store.all
   end
 end
