fb13cca994d7434797aa32268358c284c4805eea
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-09-15 19:49:25 +0200

    [factory] creates factory for store and adds test for returning all stores

--- spec/api/store_spec.rb
+++ spec/api/store_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'factories/store_factory'
 
 RSpec.describe 'Store API' do
   describe 'GET /stores' do
@@ -6,6 +7,13 @@ RSpec.describe 'Store API' do
       get ('/stores')
       expect(response).to have_http_status(200)
     end
+
+    it 'returns all stores' do
+      StoreFactory.create_stores(3)
+      get ('/stores')
+      json = JSON.parse(response.body)
+      expect(json['stores'].size).to eq(3)
+    end
+
   end
-  
 end
---
+++ spec/factories/store_factory.rb
@@ -0,0 +1,33 @@
+module StoreFactory
+  POSSIBLE_TITLES = ["Fender", "Gibson", "Hofner", "Steinway"]
+
+  POSSIBLE_ADDRESSES = [
+    { city: "Paris",  street: "Gambetta 12" },
+    { city: "Paris",  street: "Crimee 44" },
+    { city: "Berlin", street: "Weichselstr 16" },
+    { city: "Berlin", street: "Gustav-Mahler-str 31" },
+    { city: "Athens", street: "Favierou 41" },
+    { city: "Athens", street: "Leonidou 8" },
+  ]
+
+  def self.create_stores(count, explicit_params={})
+    stores = []
+    1.upto(count) do |n|
+      Store.create(store_params(explicit_params))
+    end
+    stores
+  end
+
+  def self.store_params(explicit_params)
+    address = POSSIBLE_ADDRESSES[rand(0..5)]
+    title   = POSSIBLE_TITLES[rand(0..3)]
+
+    params = { title: title,
+               city: address[:city],
+               street: address[:street]
+             }
+
+    params.merge(explicit_params)
+  end
+
+end
