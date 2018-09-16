fda52e6878e6eefbbd841015491c9716083bd514
Ahmed Tarek <ahmed.t.abdelfattah@gmail.com>
2018-09-16 16:08:50 +0200

    [routes] adds custom route for spaces price

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  match 'spaces/:id/price/:start_date/:end_date', to: 'spaces#price', via: 'get'
+
   resources :stores
   resources :spaces
 
